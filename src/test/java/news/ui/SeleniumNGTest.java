535cd93909dbc9bee3447e264261ebe09295c6da
ibankov <ibankov@2b325a56-9f04-4a27-8c6d-25ee33398e3d>
2015-10-05 13:50:51 +0000

    added custom reporter and selenium login test
    git-svn-id: http://svn.bul-si.bg/bulsisvn/projects/ProofOfConcept/NewsSample@29113 2b325a56-9f04-4a27-8c6d-25ee33398e3d

--- src/test/java/news/ui/SeleniumNGTest.java
+++ src/test/java/news/ui/SeleniumNGTest.java
@@ -16,56 +16,75 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class SeleniumNGTest {
+	
 	protected static String gridHubUrl;
 	protected static String baseUrl;
 	protected static Capabilities capabilities;
-
+	
 	private WebDriver browser;
 	private HomePage homePage;
 	private LoginPage loginPage;
-	private By loginLocator = By
-			.xpath(".//*[@id='j_idt7:j_idt8']/ul/li[6]/a/span");
-
-	@Test(description = "Login with valid data")
+	private By loginLocator = By.xpath(".//*[@id='j_idt7:j_idt8']/ul/li[3]/a/span");
+	private By userLocator = By.xpath(".//*[@id='j_idt7:j_idt8']/ul/li[6]/a/span");
+	
+	@Test(description = "Login with valid data", priority = 2)
 	public void loginTest() {
 		browser.get(baseUrl + "news/index.xhtml");
 		homePage = new HomePage(browser);
 		browser.findElement(loginLocator).click();
+		Assert.assertEquals(browser.getCurrentUrl().split(";")[0], baseUrl + "news/login.xhtml");
 		loginPage = new LoginPage(browser);
 		loginPage.loginAs("admin1", "admin1");
+		String user = browser.findElement(userLocator).getText();
+		Assert.assertEquals(user, "User:admin1");
 		Assert.assertEquals(browser.getCurrentUrl(), homePage.getUrl());
 
 	}
-	@Test(description = "Login with invalid data")
+	
+	@Test(description = "Login with invalid data",priority = 1)
 	public void invalidLoginTest() {
-
+		
 		browser.get(baseUrl + "news/index.xhtml");
 		homePage = new HomePage(browser);
 		browser.findElement(loginLocator).click();
+		Assert.assertEquals(browser.getCurrentUrl().split(";")[0], baseUrl + "news/login.xhtml");
 		loginPage = new LoginPage(browser);
 		loginPage.loginAs("admin2", "admin2");
-		Assert.assertEquals(browser.getCurrentUrl(), loginPage.getUrl());
-
+		String message = browser.findElement(By.xpath(".//*[@id='msgs']/div/ul/li")).getText();
+		Assert.assertEquals(message, "wrong username or password");
+		Assert.assertEquals(browser.getCurrentUrl(), homePage.getUrl());
 	}
-
+	
+	@Test(description = "Failing test", groups = {"fail"})
+	public void failTest() {
+		
+		Assert.assertEquals(1, 2);
+	}
+	
+	@Test(description = "Skipped test" ,dependsOnGroups = {"fail"} )
+	public void skipTest() {
+		
+		Assert.assertEquals(1, 2);
+	}
+	
 	@BeforeClass(alwaysRun = true)
 	public void setupBeforeSuite(ITestContext context) {
 		baseUrl = "http://localhost:8080/";
-
+		
 		try {
-
+			
 			browser = new FirefoxDriver();
-			browser.manage().timeouts()
-					.implicitlyWait(30, TimeUnit.SECONDS);
-		} catch (Exception e) {
+			browser.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		}
+		catch (Exception e) {
 			throw new IllegalStateException("Can't start Web Driver", e);
 		}
 	}
-
+	
 	@AfterClass
 	public void afterClass() {
 		browser.close();
 		browser.quit();
 	}
-
+	
 }
